b7de06433749252a724b2daceb22710c6360deed
PPRomaa <popadinetsroman1@gmail.com>
2024-12-03 01:41:06 +0100

    updated code, add new features

--- src/components/lastSection/LastSection.tsx
+++ src/components/lastSection/LastSection.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation} from 'swiper/modules';
 
-import "./lastSection.css"
 
 import LinkComponent from "../links/LinkComponent";
-import TeamWork from '../../icons/team-work.png';
-import HandShake from '../../icons/handshake.png';
-import LightBulb from '../../icons/light-bulb.png';
+import TeamWork from '../../icons/collaboration-100.png';
+import HandShake from '../../icons/handshake-1.png';
+import LightBulb from '../../icons/light-bulb-123.png';
+
+import "./lastSection.css"
+import "swiper/css";
+import "swiper/css/navigation";
 
 interface QuestionsArr {
     id: number;
@@ -16,17 +21,17 @@ interface QuestionsArr {
 const questionsArray: QuestionsArr[] = [
     {
         id: 0,
-        icon: <img src={LightBulb} alt="LightBulb" />,
+        icon: <img src={LightBulb} alt="LightBulb"/>,
         question: 'I bring a strong combination of technical expertise, attention to detail, and a collaborative mindset.',
     },
     {
         id: 1,
-        icon: <img src={HandShake} alt="HandShake" />,
+        icon: <img src={HandShake} alt="HandShake"/>,
         question: 'My ability to work effectively in teams, coupled with my passion for front-end development, makes me an asset for any project.',
     },
     {
         id: 2,
-        icon: <img src={TeamWork} alt="Team Work" />,
+        icon: <img src={TeamWork} alt="Team Work"/>,
         question: 'I’m always ready to take on new opportunities and contribute to innovative solutions in the web development space.',
     },
 ]
@@ -38,20 +43,37 @@ const LastSection: React.FC = () => {
                 <div className="question-tittle">
                     <h1>Why Choose Me?</h1>
                 </div>
-                <div className="question-list">
-                    {questionsArray.map(({icon, question, id}) => (
-                        <div key={id} className={'question-list-item'}>
-                            <div className={'question-item-title'}>
-                                {icon}
-                            </div>
-                            <div className={'question-item-body'}>
-                                {question}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <LinkComponent style={{justifyContent:"center"}}/>
             </div>
+            <div className="question-list">
+                <Swiper
+                    modules={[Navigation]}
+                    slidesPerView={1}
+                    spaceBetween={20}
+                    pagination={{clickable: true}}
+                    navigation
+                    style={{width: "100%"}}
+                    breakpoints={{
+                        800: {
+                            slidesPerView: 3,
+                        },
+                    }}
+                >
+                    {questionsArray.map(({icon, question, id}) => (
+                        <SwiperSlide key={id} style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            paddingTop: "20px"
+                        }}>
+                            <div className="question-list-item">
+                                <div className="question-item-title">{icon}</div>
+                                <div className="question-item-body">{question}</div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <LinkComponent style={{justifyContent: "center", paddingTop: "20px"}}/>
         </section>
     )
 }
