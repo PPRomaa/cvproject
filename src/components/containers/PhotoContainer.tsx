ec4b60d47e6bd1d5805b91a33b601f885bc4cfc9
PPRomaa <popadinetsroman1@gmail.com>
2025-02-13 22:17:41 +0100

    Reviewed code, update code add new features

--- src/components/containers/PhotoContainer.tsx
+++ src/components/containers/PhotoContainer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import image from "../../assets/images/my-image.png"
 import "./containerPhoto.css"
 
 const PhotoContainer:React.FC = () => {
     return (
         <section className="main-photo-container">
-            <div className="photo-container">
-                <img src={image} alt="my-image" />
+            <div className="photo-section">
+                <div className="photo-container">
+                </div>
             </div>
         </section>
     )
